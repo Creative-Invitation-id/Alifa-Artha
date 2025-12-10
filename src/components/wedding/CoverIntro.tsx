55ba8f76c6cd53628c3171ead6e07127590ac255
ucup233 <sufhidayat9@gmail.com>
2025-12-10 18:10:48 +0700

    correction

--- src/components/wedding/CoverIntro.tsx
+++ src/components/wedding/CoverIntro.tsx
@@ -8,15 +8,15 @@ export const CoverIntro: React.FC<CoverIntroProps> = ({ onComplete }) => {
   const [animating, setAnimating] = useState(false);
 
   useEffect(() => {
-    // Start animation after a brief moment
+    // Start animation after 1s
     const startTimer = setTimeout(() => {
       setAnimating(true);
-    }, 1200);
+    }, 1000);
 
-    // Call onComplete after animation finishes
+    // Complete after animation ends (4.8s)
     const completeTimer = setTimeout(() => {
       onComplete();
-    }, 3800);
+    }, 1000 + 4800);
 
     return () => {
       clearTimeout(startTimer);
@@ -25,12 +25,16 @@ export const CoverIntro: React.FC<CoverIntroProps> = ({ onComplete }) => {
   }, [onComplete]);
 
   return (
-    <div className="absolute inset-0 z-[100] pointer-events-none">
-      {/* Top cover - z-index lower */}
-      <div 
-        className={`absolute left-0 top-0 w-full h-1/2 z-[100] transition-transform duration-[2000ms] ease-[cubic-bezier(0.25,0.1,0.25,1)] ${
-          animating ? '-translate-y-full' : 'translate-y-0'
-        }`}
+    <div className="absolute inset-0 z-[100] pointer-events-none overflow-hidden">
+
+      {/* TOP COVER */}
+      <div
+        className={`
+          absolute left-0 top-0 w-full h-full
+          transition-transform duration-[4800ms]
+          ease-[cubic-bezier(0.17,0.55,0.55,1)]
+          ${animating ? '-translate-y-full' : 'translate-y-0'}
+        `}
       >
         <img
           src="/cover-top.svg"
@@ -39,11 +43,14 @@ export const CoverIntro: React.FC<CoverIntroProps> = ({ onComplete }) => {
         />
       </div>
 
-      {/* Bottom cover - z-index higher (on top) */}
-      <div 
-        className={`absolute left-0 bottom-0 w-full h-1/2 z-[101] transition-transform duration-[2000ms] ease-[cubic-bezier(0.25,0.1,0.25,1)] ${
-          animating ? 'translate-y-full' : 'translate-y-0'
-        }`}
+      {/* BOTTOM COVER */}
+      <div
+        className={`
+          absolute left-0 bottom-0 w-full h-full
+          transition-transform duration-[4800ms]
+          ease-[cubic-bezier(0.17,0.55,0.55,1)]
+          ${animating ? 'translate-y-full' : 'translate-y-0'}
+        `}
       >
         <img
           src="/cover-bot.svg"
@@ -51,6 +58,7 @@ export const CoverIntro: React.FC<CoverIntroProps> = ({ onComplete }) => {
           className="w-full h-full object-cover"
         />
       </div>
+
     </div>
   );
 };
